4ec4adfa21676daafe6764c6930ed3036cea3858
sharifah-nur <hadiyasharifahnur@gmail.com>
2025-01-20 11:06:13 +0000

    membuat tampilan daftar kategori

---
+++ kategori.php
@@ -0,0 +1,63 @@
+<?php
+//memasukkan header halaman
+include '.includes/header.php';
+//menyertakan file untuk menampilkan notifikasi (jika ada)
+include '.includes/toast_notification.php';
+?>
+
+<div class="container-xxl flex-grow-1 container-p-y">
+    <!-- Tabel data kategori -->
+<div class="card">
+    <div class="card-header d-flex justify-content-between align-items-center">
+        <h4>Data Kategori</h4>
+        <!-- Tombol untuk menambah kategori baru --> 
+         <button type="button" class="btn btn-primary" data-bs-toogle="modal" data-bs-target="#addCategory">
+            Tambah Kategori 
+         </button>
+    </div>
+
+    <div class="card-body">
+        <div class="table-responsive text-nowrap">
+        <table id="datatable" class="table table-hover">
+            <thead>
+                <tr class="text-center">
+                    <th width="50px"></th>
+                    <th>Nama</th>
+                    <th width="150px">Pilihan</th>
+                </tr>
+            </thead>
+            <tbody class="table-border-bottom-0">
+                <!-- Mengambil data kategori dari database -->
+                 <?php
+                 $index = 1;
+                 $query = "SELECT * FROM categories";
+                 $exec = mysqli_query($conn, $query);
+                 while ($category = mysqli_fetch_assoc($exec)) :
+                 ?>
+                 <tr>
+                    <!-- Menampilkan nomor, nama ketegori, dan opsi -->
+                    <td><?= $index++; ?></td>
+                    <td><?= $category['category_name']; ?></td>
+                    <td>
+                        <!-- Dropdown untuk opsi edit dan delete -->
+                         <div class="dropdown">
+                            <button type="button" class="btn p-0 dropdown-toogle hide-arrow" data-bs-toogle="dropdown">
+                                <i class="bx bx-dots-vertical-rounded"></i>
+                            </button>
+                            <div class="dropdown-menu">
+                                <a href="#" class="dropdown-item" data-bs-toogle="modal" data-bs-target="#editCategory_<?= $category['category_id']; ?>"><i class="bx bx-edit-alt me-2"></i>Edit</a>
+                                <a href="#" class="dropdown-item" data-bs-toogle="modal" data-bs-target="#deleteCategory_<?= $category['category_id']; ?>"><i classs="bx bx-trash me-2"></i>Delete</a>
+                            </div>
+                         </div>
+                    </td>
+                 </tr>
+                    <!-- Modal untuk hapus data kategori -->
+                    <!-- Modal untuk update data kategori -->
+                    <?php endwhile; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</div>
+<?php include '.includes/footer.php'; ?>
